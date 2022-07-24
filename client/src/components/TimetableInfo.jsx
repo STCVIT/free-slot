e08574452d799a1bff2dc4dd6f68193dadc12c36
sharpMW <saarimmw@gmail.com>
2022-07-24 15:25:44 +0530

    Login/Signup initiated

---
+++ client/src/components/TimetableInfo.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import timetableInfoImg from '../assets/TimetableInfoImage.svg'
+import fileUpload from '../assets/file-upload.svg'
+
+export default function TimeTableInfo({ formData, setFormData }) {
+    return(
+        <div className='signup-container flex items-center'>
+            <img src={timetableInfoImg} alt="signup" class="signup-image"/>
+            <div className='info-container flex-row items-center justify-center'>
+                <h1>Sign-Up</h1>
+                <p>Upload the full screenshot of your VIT Timetable of current semester.</p>
+                <form class='bg-whiteshadow-md rounded px-8 pt-6 pb-8 mb-4'>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Timetable  
+                        </label>
+                        <div> 
+                        <label class="text-center p-4 flex flex-col w-full h-full border-4 border-blue-200 border-dashed">
+                            <div>
+                                <img src ={fileUpload} alt='upload'></img>
+                                <p>Drop your Timetable here</p>
+                            </div>
+                            <div class="relative py-4">
+                                <div class="absolute inset-0 flex items-center">
+                                    <div class="w-full border-b border-gray-300"></div>
+                                </div>
+                                <div class="relative flex justify-center">
+                                    <span class="bg-white px-4 text-sm text-gray-500">or</span>
+                                </div>
+                            </div>
+                            <button className='border py-4'> Select from Computer</button>
+                        </label>
+                        <div class="relative py-4">
+                                <div class="absolute inset-0 flex items-center">
+                                    <div class="w-full border-b border-gray-300"></div>
+                                </div>
+                                <div class="relative flex justify-center">
+                                    <span class="bg-white px-4 text- text-gray-500">OR</span>
+                                </div>
+                            </div>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Paste your Timetable here" />
+                        </div>
+                    </div>
+                    <div className='flex flex-col'>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={()=>{
+                            alert("FORM SUBMITTED")
+                            console.log(formData)
+                    }}>
+                        Sign Up
+                    </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
