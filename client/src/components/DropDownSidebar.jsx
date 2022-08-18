4d112f9d1e30d5d97fc6693e3702793b18d9980c
Mehul <mehulrastogi03@gmail.com>
2022-08-18 19:53:40 +0530

    Made modals, tabs and fixed dropdown

--- client/src/components/DropDownSidebar.jsx
+++ client/src/components/DropDownSidebar.jsx
@@ -1,18 +1,30 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
+import Plus from '../Plus.svg'
+import List from '../List.svg'
+import Link from '../Link.svg'
+import ModalNewTeam from "./ModalNewTeam";
+import { useState } from "react";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
 export default function Example() {
+    const [modalOn, setModalOn] = useState(false);
+    const [choice, setChoice] = useState(false)
+  
+    const clicked = () => {
+      setModalOn(true)
+    }
     return (
+        <>
         <Menu as="div" className="relative inline-block text-left">
             <div>
                 <Menu.Button className="">
-                <p className=' justify-center items-center rounded-full bg-blueTheme p-5 px-8 tracking-wider text-slate-100'>+  Create</p>
-                   
+                    <p className=' justify-center items-center rounded-full bg-blueTheme p-5 px-8 tracking-wider text-slate-100'>+  Create</p>
+
                 </Menu.Button>
             </div>
 
@@ -25,65 +37,68 @@ export default function Example() {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-0 focus:outline-none">
-                    <div className="py-1">
+                <Menu.Items className="origin-top-right absolute left-0 mt-2 w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-0 focus:outline-none">
+                    <div className="py-1 p-2">
                         <Menu.Item>
                             {({ active }) => (
-                                <a
-                                    href="/"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    Account settings
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="/"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    Support
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="/"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    License
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <form method="POST" action="/">
-                            <Menu.Item>
-                                {({ active }) => (
-                                    <button
-                                        type="submit"
-                                        className={classNames(
-                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                            'block w-full text-left px-4 py-2 text-sm'
-                                        )}
-                                    >
-                                        Sign out
+                                
+                                <>
+                                    <button className="flex px-6 items-center p-2" onClick={clicked} >
+                                        <img src={Plus} alt='' className='h-4 w-4' />
+                                        <p
+                                            
+                                            className={classNames(
+                                                active ? 'text-sm bg-gray-100 text-gray-900 ' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )} 
+                                        >
+                                            Make new team
+                                        </p>
                                     </button>
-                                )}
-                            </Menu.Item>
-                        </form>
+                                   
+                                </>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item>
+                            {({ active }) => (
+                                <>
+
+                                    <div className="flex px-6 items-center p-2">
+                                        <img src={List} alt='' className='h-4 w-4 ' />
+                                        <p
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900 ' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}>
+                                            Choose from existing
+                                        </p>
+                                    </div>
+
+                                </>
+                            )}
+                        </Menu.Item>
+                        <Menu.Item>
+                            {({ active }) => (
+                                <>
+                                    <div class="flex px-6 items-center p-2">
+                                        <img src={Link} alt='' className='h-4 w-4' />
+                                        <p
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Create link
+                                        </p>
+                                    </div>
+                                </>
+                            )}
+                        </Menu.Item>
                     </div>
                 </Menu.Items>
             </Transition>
         </Menu>
+        {modalOn && < ModalNewTeam setModalOn={setModalOn} setChoice={setChoice} />}
+        </>
     )
 }
--- client/src/components/Tabs.jsx
+++ client/src/components/Tabs.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import { useState } from "react";
+import TabsData from "./TabsData"
+export function Tabs() {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-function Tabs() {
   return (
-    <>
-      <div className="tabs col-span-5">
-        <p className="tab tab-bordered" >Tab 1</p>
-        <p className="tab tab-bordered tab-active" href='/'>Tab 2</p>
-        <p className="tab tab-bordered" href='/' >Tab 3</p>
+    <div className="col-span-5">
+      <div className="text-center text-4xl font-bold p-4 py-5 ">Meetings</div>
+      <div className="border-b grid grid-cols-12">
+        {/* Loop through tab data and render button for each. */}
+        {TabsData.map((tab, idx) => {
+          return (
+            <button
+              key={idx}
+              className={`py-2 border-b-4 transition-colors duration-300 ${
+                idx === activeTabIndex
+                  ? "border-blueTheme"
+                  : "border-transparent hover:border-gray-200"
+              }`}
+              // Change the active tab on click.
+              onClick={() => setActiveTabIndex(idx)}>
+              {tab.label}
+            </button>
+          );
+        })}
       </div>
-    </>
-  )
+      {/* Show active tab content. */}
+      <div className="py-4">
+        <p>{TabsData[activeTabIndex].content}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Tabs
+export default Tabs;
