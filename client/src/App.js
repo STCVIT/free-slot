9b01b6d8ca7fc46b3b76da628aa6287f0e301f60
Dev Mehta <dev.a.mehta30@gmail.com>
2022-09-15 00:50:42 +0530

    fix: App.js and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,17 @@
-<<<<<<< HEAD
-// import Login from './components/Login/Login'
-=======
-<<<<<<< HEAD
-//import Login from './components/Login'
-=======
-import Login from './components/Login/Login'
->>>>>>> 45e23c25719eb361cf2ab6825d39e0be8989ed4a
->>>>>>> 4bb6e5cb81db83a04ddbaefab6dbe56e9574dd94
+import Login from './components/Login/Login1'
 // import SignupInfo from './components/SignupInfo'
 // import TimeTableInfo from './components/TimetableInfo';
 import Home from "./components/Home"
-//import Landing from './components/Landing'
+import Landing from './components/Landing'
 import ProtectedRoute from './components/ProtectedRoute'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { UserAuthContextProvider } from './context/UserAuthContext'
-// import card from "./components/card"
-// import Schedule from "./components/Schedule";
+import Schedule from "./components/Schedule";
 // import {Container,Row, Col } from "react-bootstrap"
-<<<<<<< HEAD
-//import SignUp from './components/Signup/SignUpForm';
-// import AddEvent from "./components/AddEvent";
-import Modify from "./components/Modify";
-
-
-
-
-=======
 import SignUp from './components/Signup/SignUpForm';
 import AddEvent from "./components/AddEvent";
 import './App.css'
-import FreeSlot from "./components/Freeslot";
 import HomePageBuild from './components/HomePageBuild'
-<<<<<<< HEAD
-import Login from "./components/Login/Login1"
 export default function App() {
   return (
     <UserAuthContextProvider>
@@ -49,54 +28,12 @@ export default function App() {
           <Route path='/addevent' element={<AddEvent />} />
           <Route path='/schedule' element={<Schedule />} />
           <Route path='/login' element={<Login />} />
-          {/* <Route path='/login1' element={<Login1 />} /> */}
           <Route path='/signup' element={<SignUp />} />
-          {/* <Route path='/freeslots' element={<Freeslot />} /> */}
           <Route path='/homepage' element={<HomePageBuild />} />
 
 
         </Routes>
       </BrowserRouter>
     </UserAuthContextProvider>
-=======
->>>>>>> 45e23c25719eb361cf2ab6825d39e0be8989ed4a
-export default function App() {
-  return (
-      <UserAuthContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/home'
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-<<<<<<< HEAD
-            {/* <Route path='/' element={<Landing />} />  */}
-            {/* <Route path="/" element={<card />} /> */}
-            {/* <Route path='/' element={<AddEvent />} /> */}
-            {/* <Route path='/' element={<Schedule />} /> */}
-            {/* <Route path='/' element={<TimeTableInfo />} /> */}
-            {/* <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} /> */}
-            <Route path='/' element={<Modify />} /> 
-            
-            
-=======
-            <Route path='/' element={<Landing />} /> 
-            <Route path='/addevent' element={<AddEvent />} />
-            <Route path='/schedule' element={<Schedule />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-            {/* <Route path='/freeslots' element={<Freeslot />} /> */}
-            <Route path='/homepage' element={<HomePageBuild />} />
-
->>>>>>> 45e23c25719eb361cf2ab6825d39e0be8989ed4a
-            
-          </Routes>
-        </BrowserRouter>
-      </UserAuthContextProvider>
->>>>>>> 4bb6e5cb81db83a04ddbaefab6dbe56e9574dd94
   );
 }
