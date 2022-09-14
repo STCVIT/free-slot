c94f9075dafabf339c76b286d1487b160af639a5
Dev Mehta <dev.a.mehta30@gmail.com>
2022-09-15 00:37:01 +0530

    fix: Login Page completed

---
+++ client/src/components/Login/Login1.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../../context/UserAuthContext";
+import googleLogo from "../../assets/Gooogle-logo.svg";
+import LoginImage from "../../assets/LoginInfoImage.svg";
+import Visible from "../../assets/fi_eye.svg";
+import NotVisible from "../../assets/fi_eye-off.svg";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { signIn, googleSignIn } = UserAuth();
+  const [passwordType, setPasswordType] = useState("password");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signIn(email, password);
+      navigate("/home");
+    } catch (error) {
+      setError(e.message);
+      console.log(e.message);
+    }
+  };
+  const oAuth = async (e) => {
+    setError("");
+    try {
+      await googleSignIn();
+      navigate("/home");
+    } catch (error) {}
+  };
+  const togglePassword = (e) => {
+    e.preventDefault();
+    if (passwordType === "password") {
+      setPasswordType("text");
+      return;
+    }
+    setPasswordType("password");
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-4">
+        <div className="hidden sm:block sm:col-span-2 bg-blueTheme ">
+          <img
+            src={LoginImage}
+            alt=""
+            className="grid col-span-2 min-h-screen"
+          />
+        </div>
+        <div className="visible col-span-1 sm:col-span-2 mt-7">
+          <div className="px-3 sm:py-5 sm:px-24 md:py-7.5 md:px-36 lg:py-10 lg:px-48">
+            <h1 className="font-bold sm:text-center text-3xl pb-10">Login</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col py-2 w-full">
+                <label className="font-semibold py-2">Email</label>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  className="focus:outline-none border-2 rounded py-3 px-4"
+                />
+              </div>
+              {/* <div className="flex flex-col py-2">
+                <label className=" font-semibold py-2">Password</label>
+                <div className="flex flex-row border-2 rounded">
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    type={passwordType}
+                    value={password}
+                    className=" focus:outline-none p-2 px-4 w-full"
+                  />
+                  <img
+                    src={eye}
+                    className="p-1"
+                    alt=""
+                    onClick={togglePassword}
+                  />
+                </div>
+              </div> */}
+              <div className="flex flex-col py-2">
+                <label className=" font-semibold py-2">Password</label>
+                <div className="flex flex-row border-2 rounded">
+                  <input
+                    className=" focus:outline-none px-4 py-3 w-full"
+                    type={passwordType}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div onClick={togglePassword}>
+                    {passwordType === "password" ? (
+                      <img className="py-4 px-2" src={Visible} alt="Eye Icon" />
+                    ) : (
+                      <img
+                        className="py-4 px-2"
+                        src={NotVisible}
+                        alt="Open Eye Icon"
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
+            </form>
+            <div className="pt-8 pb-1 text-center ">
+              <button className="bg-blueTheme text-white w-full font-bold mx-auto py-3 rounded ">
+                Login
+              </button>
+            </div>
+            <h6 className="text-sm text-grey">
+              <a href="">Forgot Password?</a>
+            </h6>
+            <div className="px-12 py-8">
+              <hr />
+            </div>
+            <div className="py-5">
+              <button
+                className="flex justify-center w-full border-2 border-blueTheme text-black font-semibold mx-auto py-3 rounded "
+                type="button"
+                onClick={oAuth}
+              >
+                <img className="px-2" src={googleLogo} alt="" />
+                <p>Continue with Google</p>
+              </button>
+              <h6 className="text-sm text-grey py-2">
+                Don't have an account?
+                <a href="" className="pl-2">
+                  Sign-Up
+                </a>
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
