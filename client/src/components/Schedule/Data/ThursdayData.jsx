9a83b5bf809f429e49001d9c8bf168c0e4d1c43d
Ayush <mhetreayush1719@gmail.com>
2022-09-18 18:17:30 +0530

    day navbar and card style updated

--- client/src/components/Schedule/Data/ThursdayData.jsx
+++ client/src/components/Schedule/Data/ThursdayData.jsx
@@ -1,14 +1,8 @@
 export default [
-    {
-        time: "8.00 - 8.00 AM",
-        subject: "BEE",
-        venue: "TT-614"
-    },
-    {
-        time: "8.00 - 8.00 AM",
-        subject: "BEE",
-        venue: "TT-614"
-    },
-   
-
-]
+  {
+    time: "8.00 - 8.00 AM",
+  },
+  {
+    time: "8.00 - 8.00 AM",
+  },
+];
--- client/src/components/Schedule/SheduleButton.jsx
+++ client/src/components/Schedule/SheduleButton.jsx
@@ -1,10 +1,8 @@
 const ScheduleButton = (props) =>{
     return(
         <div class="p-2 rounded-lg" id="mon" role="tabpanel" aria-labelledby="mon-tab">
-            <div class="p-2 m-2 max-w-sm bg-white rounded-lg border border-black shadow-md">
-              <p class="mt-1 text-xs font-bold text-zinc-600">{props.time}</p>
-              <p class="font-bold text-zinc-600">{props.subject}</p>
-              <p class="mb-1 text-xs text-zinc-600">{props.venue}</p>
+            <div class="p-2 m-2 max-w-sm bg-white border-b border-[#ECECEC]">
+              <p class="font-bold text-zinc-600">{props.time}</p>
           </div>
         </div>
     )
