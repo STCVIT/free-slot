29792f4e8b96ab5750f3b825823f2272b91ed152
sharpMW <saarimmw@gmail.com>
2022-09-18 08:39:29 +0530

    pulled from ayush

--- server/src/routers/testRouter.js
+++ server/src/routers/testRouter.js
@@ -2,5 +2,5 @@ const router = require('express').Router()
 const { hello } = require('../controllers/testController')
 const { testMid } =  require('../middleware/testMiddleware')
 
-router.post('/create', testMid, hello)
+router.post('/test', hello)
 module.exports = router
