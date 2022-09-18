52d31692ed808ac0194052bd0382e74c2c5f9543
Mehul <mehulrastogi03@gmail.com>
2022-09-18 22:01:02 +0530

    data

--- client/src/components/DropDownProfile.jsx
+++ client/src/components/DropDownProfile.jsx
@@ -10,7 +10,7 @@ export default function Example() {
     return (
         <Menu as="div" className="flex items-center px-4 -mb-1">
             <div>
-                <Menu.Button className="">
+                <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <img src={profile} alt='' className='h-1/6' />
                 </Menu.Button>
             </div>
@@ -24,7 +24,8 @@ export default function Example() {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="origin-top-right absolute right-20 top-20 mt-2 w-30 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-0 focus:outline-none">
+                {/* <Menu.Items className=" absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"> */}
+                <Menu.Items className="origin-top-right absolute right-4 top-20 mt-2 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-0 focus:outline-none">
                     <div className="py-1">
                         <Menu.Item>
                             {({ active }) => (
