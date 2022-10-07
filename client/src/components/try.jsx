cb626a7ad5485f61dadc1f08857dd543bdb41456
Ayush <mhetreayush1719@gmail.com>
2022-10-07 17:48:44 +0530

    created add reg numbers component

--- client/src/components/try.jsx
+++ client/src/components/try.jsx
@@ -2,17 +2,53 @@ import { CheckBox, CheckBoxOutlineBlank } from "@mui/icons-material";
 import { useState } from "react";
 
 function TagsInput() {
+  var regex = /([0-9]{2})([A-Za-z]{3})([0-9]{4})/;
   const [tags, setTags] = useState([]);
   const [tagNote, setTagNote] = useState("Add a tag");
   function handleKeyDown(e) {
     if (e.key !== "Enter") return;
     else {
       setTagNote("Click a tag to remove.");
+      const value = e.target.value;
+      if (value.includes(",")) {
+        const duplicates = [];
+        const invalid = [];
+        e.target.value = "";
+        var tag = value.split(",");
+        tag.forEach((element) => {
+          if (regex.test(element)) {
+            if (
+              tags.find((tag) => tag.toLowerCase() === element.toLowerCase())
+            ) {
+              duplicates.push(element);
+              return;
+            }
+            tags.push(element);
+          } else {
+            invalid.push(element);
+          }
+        });
+        if (duplicates.length > 0) {
+          alert("Duplicate tags: " + duplicates.join(", "));
+        }
+        if (invalid.length > 0) {
+          alert("Invalid tags: " + invalid.join(", "));
+        }
+      } else {
+        if (!value.trim()) return;
+        else if (!regex.test(value) || value.length > 9) {
+          setTagNote("Please enter a valid tag.");
+          e.target.value = "";
+          return;
+        } else if (tags.includes(value.toUpperCase())) {
+          alert(value + " already exists!");
+          e.target.value = "";
+          return;
+        }
+        setTags([...tags, value.toUpperCase()]);
+        e.target.value = "";
+      }
     }
-    const value = e.target.value;
-    if (!value.trim()) return;
-    setTags([...tags, value]);
-    e.target.value = "";
   }
 
   function removeTag(index) {
@@ -26,67 +62,82 @@ function TagsInput() {
 
   const submitTags = () => [console.log(tags)];
   return (
-    <div className="flex flex-col gap-y-3 justify-center items-start px-2 py-6">
-      <div>
-        Enter the reg no. or name of the members:
-        <br />
-        <i>Press Enter to seperate tags.</i>
-      </div>
-      <div className="self-center">{tagNote}</div>
-      <div className="flex flex-col items-center w-[30rem] p-2 rounded-md justify-center gap-2 border">
-        <div className="flex flex-wrap items-center justify-center gap-3">
-          {tags.map((tag, index) => (
-            <button
-              onClick={() => removeTag(index)}
-              className="p-2 bg-[#CFDBE6] rounded-md hover:text-red-400 hover:line-through h hover:scale-110"
-              key={index}
-            >
-              <span className="text">{tag}</span>
-            </button>
-          ))}
-          <input
-            onKeyDown={handleKeyDown}
-            type="text"
-            className="w-fit text-center border rounded-md py-2"
-            placeholder="21XXX0000"
-          />
+    <div>
+      <div className="flex flex-col gap-y-3 justify-center items-start px-2 py-6">
+        <div>
+          Enter the reg no. or name of the members:
+          <br />
+          <i>Press Enter to seperate tags.</i>
         </div>
-        <button onClick={submitTags}>Submit</button>
+        <div className="self-center">{tagNote}</div>
+        <div className="flex flex-col items-center w-3/4 p-2 rounded-md justify-center gap-2 border self-center">
+          <div className="flex flex-wrap items-center justify-center gap-3">
+            {tags.map((tag, index) => (
+              <button
+                onClick={() => removeTag(index)}
+                className="p-2 bg-[#CFDBE6] rounded-md hover:text-red-400 hover:line-through h hover:scale-110"
+                key={index}
+              >
+                <span className="text">{tag}</span>
+              </button>
+            ))}
+            <input
+              onKeyDown={handleKeyDown}
+              type="text"
+              className="w-fit text-center border rounded-md py-2"
+              placeholder="21XXX0000"
+            />
+          </div>
+          <button onClick={submitTags}>Submit</button>
+        </div>
       </div>
-        <button className="self-center px-3 py-2 bg-red-400 rounded-md text-white font-semibold" onClick={removeAll}>Clear all</button>
+      <button
+        className="underline decoration-dotted underline-offset-2 font-semibold"
+        onClick={removeAll}
+      >
+        Clear all
+      </button>
     </div>
   );
 }
 
 const FreeSlot = () => {
-  const [saveTeam, setSaveTeam] = useState(false)
+  const [saveTeam, setSaveTeam] = useState(false);
 
   return (
-    <div className="h-screen w-screen bg-gray-300">
-      <div className="flex z-50 top-[25vh] left-[35vw] absolute justify-center flex-col gap-y-3 drop-shadow-xl bg-white rounded-md py-3 px-10">
-        <h1 className="text-center font-bold text-2xl">Check Free Slot</h1>
-        <TagsInput />
-        <div className="flex gap-x-3 items-center font-bold cursor-pointer" onClick={()=>{
-          setSaveTeam(!saveTeam)
-          console.log(saveTeam)
-          }}>
-          {saveTeam ?  <CheckBoxOutlineBlank /> : <CheckBox />}
-          Save this team
+    <div>
+      <div>asdasd</div>
+      <div className="grid grid-cols-12 bg-gray-300/70 backdrop-blur-sm absolute w-screen top-0 z-50 items-center h-screen">
+        <div className="col=span-1 md:col-span-3"></div>
+        <div className="flex justify-center flex-col gap-y-3 drop-shadow-xl bg-white rounded-md p-3 col-span-10 md:col-span-6">
+          <h1 className="text-center font-bold text-2xl">Check Free Slot</h1>
+          <TagsInput />
+          <div
+            className="flex gap-x-3 items-center font-bold cursor-pointer"
+            onClick={() => {
+              setSaveTeam(!saveTeam);
+              console.log(saveTeam);
+            }}
+          >
+            {saveTeam ? <CheckBoxOutlineBlank /> : <CheckBox />}
+            Save this team
+          </div>
+          <div className="self-center flex w-3/4 md:w-2/4 justify-between gap-x-3">
+            <button
+              type="cancel"
+              class="text-black bg-white border-none font-medium rounded-lg underline-offset-2 text-sm w-60 outline text-center underline decoration-dotted"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              class="text-white bg-blue-700 font-medium rounded-lg text-sm w-60 py-2.5 text-center dark:bg-blue-600 "
+            >
+              Find Free Slot!
+            </button>
+          </div>
         </div>
-        <div className="self-center flex w-2/4 justify-between gap-x-3">
-              <button
-                type="cancel"
-                class="text-black bg-white border-none font-medium rounded-lg text-sm w-60 outline text-center underline decoration-dotted outline-offset-0"
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                class="text-white bg-blue-700 font-medium rounded-lg text-sm w-60 py-2.5 text-center dark:bg-blue-600 "
-              >
-                Find Free Slot!
-              </button>
-            </div>
+        <div className="col-span-1 md:col-span-3"></div>
       </div>
     </div>
   );
