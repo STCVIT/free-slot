37515cc0155983fb11d13f8e6acf7d42700b0b83
sharpMW <saarimmw@gmail.com>
2022-10-07 16:35:24 +0530

    fixing bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,30 @@
-import Login from './components/Login/Login'
-import Home from "./components/Home"
-import Landing from './components/Landing'
-import ProtectedRoute from './components/ProtectedRoute'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { UserAuthContextProvider } from './context/UserAuthContext'
-import Schedule from "./components/Schedule";
-import CARD from "./components/card"
-import AddEvent from "./components/AddEvent";
-import './App.css'
-import HomePageBuild from './components/HomePageBuild'
+import ProtectedRoute from './components/ProtectedRoute'
+
+//components
 import Signup from './components/Signup/SignUp'
-import TT from './components/Signup/Timetable'
+import Timetable from './components/Signup/Timetable'
+import Login from './components/Login/Login'
+import Home from "./pages/Home"
+import Landing from './components/Landing'
+import Schedule from "./components/Schedule";
+import AddEvent from "./components/AddEvent";
+
+import './App.css'
 
 export default function App() {
   return (
     <UserAuthContextProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/home'
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
           <Route path='/' element={<Landing />} />
-          <Route path='/addevent' element={<AddEvent />} />
-          <Route path='/schedule' element={<Schedule />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<ProtectedRoute> <Home /></ProtectedRoute>}/>
           <Route path='/signup' element={<Signup />} />
-          <Route path='/timetable' element={<TT />} />
-          <Route path='/homepage' element={<HomePageBuild />} />
-          <Route path='/card' element={<CARD />} />
+          <Route path='/timetable' element={<Timetable />}/>
+          <Route path='/login' element={<Login />} />
+          <Route path='/addevent' element={<ProtectedRoute><AddEvent /></ProtectedRoute>} />
+          <Route path='/schedule' element={<ProtectedRoute><Schedule /></ProtectedRoute>} />
         </Routes>
       </BrowserRouter>
     </UserAuthContextProvider>
