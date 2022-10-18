327e1d8261853f6ba85a0d2ba2629894b10c8f3e
Ayush <mhetreayush1719@gmail.com>
2022-10-19 02:11:39 +0530

    Fix navbar and Responses page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,71 +1,46 @@
 import React from "react";
-import profile from "../../assets/Profile.svg"
-import DropDownProfile from "../DropDownProfile"
-import {NavButton} from "./NavButton/NavButton";
+import profile from "../../assets/Profile.svg";
+import DropDownProfile from "../DropDownProfile";
+import { NavButton } from "./NavButton/NavButton";
 
 function Navbar() {
-    const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
 
-    return (
-        <>
-            <header className="shadow z-20">
-                <div className="flex items-center md:flex-row flex-col justify-between w-full">
-                    <div>
-                    <p className="font-logo flex items-center w-full p-7 md:p1 text-5xl ">
-                        Free Slots
-                    </p>
-                    </div>
-                    <div>
+  return (
+    <>
+      <header className="shadow z-20">
+        <div className="flex items-center md:flex-row flex-col justify-between w-full">
+          <div>
+            <p className="font-logo flex items-center w-full p-7 md:p1 text-5xl ">
+              Free Slots
+            </p>
+          </div>
+          <div>
+            <ul className="flex">
+              <NavButton />
+              <li className="flex ">
+                <DropDownProfile />
+              </li>
+            </ul>
+          </div>
+        </div>
 
-                    <ul className="flex">
-                        {/* <li className="flex" >
-                            <p className="flex items-center px-4 -mb-1">Home</p>
-                            
-                            </li>
-                            <li className="flex">
-                            <p className="flex items-center px-4 -mb-1">My Schedule</p>
-                            </li>
-                            <li className="flex">
-                            <p className="flex items-center px-4 -mb-1">About</p>
-                        </li> */}
-                        <NavButton />
-                        <li className="flex ">
-                            <DropDownProfile />
-                        </li>
-                        
-                    </ul>
-                        </div>
-                </div>
-
-                <div className="md:hidden">
-                    <ul className={(navbarOpen ? "grid grid-cols-1" : "hidden")}>
-                        <li ><p className="block " >Help</p></li>
-                        <li ><p className="block " >About</p></li>
-                        <li ><p className="block ">Contact</p></li>
-                    </ul>
-                </div>
-            </header>
-
-            {/* <nav className='flex'>
-                <div className='flex-none'>
-                    <h1 className=' text-7xl p-5'>FreeSlots</h1>
-                </div>
-                <div className='flex flex-row-reverse'>
-                    <div >
-                        <h3 className=' p-5'>Home</h3>
-                    </div>
-                    <div>
-                        <h3 className=' p-5'>My Schedule</h3>
-                    </div>
-                    <div>
-                        <h3 className=' p-5'>About</h3>
-                    </div>
-                </div>
-            </nav> */}
-        </>
-
-    )
+        <div className="md:hidden">
+          <ul className={navbarOpen ? "grid grid-cols-1" : "hidden"}>
+            <li>
+              <p className="block ">Help</p>
+            </li>
+            <li>
+              <p className="block ">About</p>
+            </li>
+            <li>
+              <p className="block ">Contact</p>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
 }
 
-
-export default Navbar
+export default Navbar;
