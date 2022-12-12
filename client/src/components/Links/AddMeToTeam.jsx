c5069f3dd1fc92a0846f4f74ffe6d455771eb674
sharpMW <saarimmw@gmail.com>
2022-12-12 22:49:06 +0530

    added acceptHandler and rejectHandler

--- client/src/components/Links/AddMeToTeam.jsx
+++ client/src/components/Links/AddMeToTeam.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from '../../axios'
 import { FindFreeSlot } from '../../context/FreeSlotContext'
 
 const AddMeToTeam = () => {
   const { linkCreator, linkTeam} = FindFreeSlot()
-  const acceptHandler=()=>{
-
+  const navigate = useNavigate()
+  const uid = window.location.pathname.match(/[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}/g)
+  const user = JSON.parse(localStorage.getItem("user"))
+  const acceptHandler= async()=>{
+    const res = await axios.post('user/getUser', {
+      email: user.email
+    })
+    if(res){
+      console.log(res)
+      const secondResponse = await axios.patch('team/updateTeam/'+uid, {
+        members: res.data.reg_no
+      })
+      if(secondResponse){
+        //make a success pop up and redirect to home with timeout
+      }
+    }
   }
-  const rejectHandler=()=>{
-
+  const rejectHandler= async()=>{
+    //make a reject popup and redirect to home
   }
   return (
     <>
