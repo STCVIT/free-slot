74bdd488d7d2d60dc9fe495d914c54132f49f9ed
Ayush <mhetreayush1719@gmail.com>
2022-12-14 21:31:22 +0530

    deployment trials

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,39 +1,42 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const config = require('./config')[process.env.NODE_ENV || 'development']
+const { Sequelize, DataTypes } = require("sequelize");
+const config = require("./config")[process.env.NODE_ENV || "development"];
 //for locally running
-const sequelize = new Sequelize(config.postgres)
+// const sequelize = new Sequelize(config.postgres)
 
-//for hosting
-// const sequelize= new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//     }
-// } })
-sequelize.authenticate()
-    .then(()=>{
-        console.log('Connected successfully')
-    })
-    .catch(()=>{
-        console.log('Couldn\'t connect to database')
-    })
-const db = {}
-db.Sequelize = Sequelize
-db.sequelize = sequelize
-db.users=require('../models/user.model')(sequelize, DataTypes)
-db.teams=require('../models/team.model')(sequelize, DataTypes)
-db.meets=require('../models/meetings.model')(sequelize, DataTypes)
-db.teams.hasMany(db.meets, { foreignKey: "team_id" })
-db.meets.belongsTo(db.teams, {foreignKey: "meet_id"})
-db.teams.belongsToMany(db.users, {through: 'userteams'})
-db.users.belongsToMany(db.teams, {through: 'userteams'})
-sequelize.sync({ alter: true })
-    .then(()=>{
-        console.log("Database, tables & associations created")
-    })
-    .catch((err)=>{
-        console.error(err.message)
-    }) 
-module.exports = db
+// for hosting
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected successfully");
+  })
+  .catch(() => {
+    console.log("Couldn't connect to database");
+  });
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.users = require("../models/user.model")(sequelize, DataTypes);
+db.teams = require("../models/team.model")(sequelize, DataTypes);
+db.meets = require("../models/meetings.model")(sequelize, DataTypes);
+db.teams.hasMany(db.meets, { foreignKey: "team_id" });
+db.meets.belongsTo(db.teams, { foreignKey: "meet_id" });
+db.teams.belongsToMany(db.users, { through: "userteams" });
+db.users.belongsToMany(db.teams, { through: "userteams" });
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Database, tables & associations created");
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+module.exports = db;
