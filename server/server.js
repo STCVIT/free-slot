17053e00d4230ed55d52425e54c1268298ae41b4
sharpMW <saarimmw@gmail.com>
2022-12-20 17:31:26 +0530

    railway deploy v6

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const app = require('./src/app')
+const expose = '0.0.0.0:'
 const port = process.env.PORT || 4000;
+const exposedPort=expose+port
+
 
 app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${exposedPort}`)
 })
